ecb83427bbe9538064c6b0a5dbffd5060921797f
hexi <xi.he@baidao.com>
2016-10-26 10:53:57 +0800

    Add stetho_realm

--- realm-intro/src/main/java/com/baidao/realmtest/MyApplication.java
+++ realm-intro/src/main/java/com/baidao/realmtest/MyApplication.java
@@ -2,6 +2,9 @@ package com.baidao.realmtest;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
 import io.realm.Realm;
 
 /**
@@ -15,5 +18,11 @@ public class MyApplication extends Application {
         super.onCreate();
         // Initialize Realm. Should only be done once when the application starts.
         Realm.init(this);
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
     }
 }
